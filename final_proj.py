03c363d13b76d736df8a8c9e572ecc8ed525a1ac
aidanM1776 <107961189+aidanM1776@users.noreply.github.com>
2026-04-27 01:07:57 -0500

    Compute and plot PSD for sleep epochs
    Added PSD and more data

--- final_proj.py
+++ final_proj.py
@@ -22,8 +22,7 @@ raw2.set_channel_types({'EOG horizontal':'eog','EMG submental':'emg'})
 raw3 = mne.io.read_raw_edf('final_proj/SC4011E0-PSG.edf', exclude=('Resp oro-nasal', 'Temp rectal','Event marker'))
 raw3.set_channel_types({'EOG horizontal':'eog','EMG submental':'emg'})
 
-# the hypnogram.edf file only has annotations
-# this will attach the correct stage to the raw object
+# the hypnogram.edf file only has annotations, must add to raw data
 ann1 = mne.read_annotations('final_proj/SC4001EC-Hypnogram.edf')
 raw1.set_annotations(ann1)
 
@@ -34,19 +33,22 @@ ann3 = mne.read_annotations('final_proj/SC4011EH-Hypnogram.edf')
 raw3.set_annotations(ann3)
 
 raw = mne.concatenate_raws([raw1, raw2, raw3])
+
 # Epoch object has an (X,Y,Z) numpy matrix
 # X: number of epochs (each epoch is 30 seconds as specified below)
 # Y: number of channels (the EDF data has 4 channels)
 # Z: data points in the 30 second intervals
 epochs = mne.make_fixed_length_epochs(raw, duration=30.0, preload=True)
 
+psd = epochs.compute_psd(fmin=0.5, fmax=30, picks=['eeg', 'eog', 'emg'])
 
-# get labels and
+# get labels and match length to epochs
 labels = raw.annotations.description
 n = min(len(epochs), len(labels))
 epochs = epochs[:n]
 labels = labels[:n]
 
+# encoding hypnogram labels
 class_map = {
     'Sleep stage W': 0,
     'Sleep stage 1': 1,
@@ -61,4 +63,29 @@ Y = np.array([class_map.get(i,i) for i in labels])
 
 print(X.shape)
 print(Y.shape)
-epochs.plot(n_epochs=154, n_channels=4, block=True)
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+#epochs.plot(n_epochs=436, n_channels=4)
+#psd.plot(spatial_colors=True, dB=True)
+
+# Plot of first epoch (30 seconds) for 4 channels over time
+data = X[0]
+times = epochs.times
+ax1.plot(times, data.T)
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Amplitude (µV)')
+ax1.set_title('First Epoch')
+ax1.legend(epochs.ch_names)
+
+
+# Plotting psd of first epoch
+psd_data, freqs = psd.get_data(picks=['eeg', 'eog', 'emg'],return_freqs=True)
+psd_db = 10 * np.log10(psd_data[0])
+ax2.plot(freqs, psd_db.T)
+ax2.set_xlabel('Frequency (Hz)')
+ax2.set_ylabel('Amplitude')
+ax2.set_title('First Epoch')
+ax2.legend(psd.ch_names)
+
+plt.tight_layout()
+plt.show()
