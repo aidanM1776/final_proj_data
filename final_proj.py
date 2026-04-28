7bdedd518212919fc97c52b7ca6c530cfbbd244d
aidanM1776 <107961189+aidanM1776@users.noreply.github.com>
2026-04-28 16:10:21 -0500

    I dont even know what this boost stuff is doing
    please fix

--- final_proj.py
+++ final_proj.py
@@ -64,6 +64,13 @@ raw2.set_channel_types({'EOG horizontal':'eog','EMG submental':'emg'})
 raw3 = mne.io.read_raw_edf(f'{project_destination}/SC4011E0-PSG.edf', exclude=('Resp oro-nasal', 'Temp rectal','Event marker'))
 raw3.set_channel_types({'EOG horizontal':'eog','EMG submental':'emg'})
 
+raw4 = mne.io.read_raw_edf(f'{project_destination}/SC4012E0-PSG.edf', exclude=('Resp oro-nasal', 'Temp rectal','Event marker'))
+raw4.set_channel_types({'EOG horizontal':'eog','EMG submental':'emg'})
+
+## Testing data not used for training the model, just to see how it performs on new data
+test_raw1 = mne.io.read_raw_edf(f'{project_destination}/SC4021E0-PSG.edf', exclude=('Resp oro-nasal', 'Temp rectal','Event marker'))
+test_raw1.set_channel_types({'EOG horizontal':'eog','EMG submental':'emg'})
+
 # the hypnogram.edf file only has annotations, must add to raw data
 ann1 = mne.read_annotations(f'{project_destination}/SC4001EC-Hypnogram.edf')
 raw1.set_annotations(ann1)
@@ -74,7 +81,16 @@ raw2.set_annotations(ann2)
 ann3 = mne.read_annotations(f'{project_destination}/SC4011EH-Hypnogram.edf') # make sure that after SC4011 it's H NOT C !!
 raw3.set_annotations(ann3)
 
-raw = mne.concatenate_raws([raw1, raw2, raw3])
+ann4 = mne.read_annotations(f'{project_destination}/SC4012EC-Hypnogram.edf')
+raw4.set_annotations(ann4)
+
+## For the test data
+test_ann1 = mne.read_annotations(f'{project_destination}/SC4021EH-Hypnogram.edf')
+test_raw1.set_annotations(test_ann1)
+
+
+raw = mne.concatenate_raws([raw1, raw2, raw3, raw4]) # Data used to train the model
+raw.resample(100.0, npad='auto')  # lower sampling rate before epoching to speed processing
 
 # Epoch object has an (X,Y,Z) numpy matrix
 # X: number of epochs (each epoch is 30 seconds as specified below)
@@ -128,7 +144,7 @@ valid_mask = [lbl in class_map for lbl in epoch_labels]
 X_data = X_data[valid_mask]
 y = np.array([class_map[lbl] for lbl in epoch_labels if lbl in class_map], dtype=int)
 times = epoch_midpoints[valid_mask]
-      
+
 # Statistics for each channel in each epoch
 feature_rows = []
 for idx, epoch in enumerate(X_data):
@@ -143,9 +159,10 @@ for idx, epoch in enumerate(X_data):
     feature_rows.append(row)
 
 features_df = pd.DataFrame(feature_rows)
+TRAINING_FEATURE_COLUMNS = features_df.drop(columns=['time']).columns.tolist()
 
 # Train-test split and XGBoost classifier
-X_train, X_test, y_train, y_test = train_test_split(features_df.drop(columns=['time']), y, test_size=0.3, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(features_df.drop(columns=['time']), y, test_size=0.5, random_state=42, stratify=y)
 model = XGBClassifier(use_label_encoder=False, eval_metric='mlogloss', random_state=42)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
@@ -155,29 +172,53 @@ print(classification_report(y_test, y_pred))
 
 ## Cross-validation with XGBoost and StratifiedKFold
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-model = XGBClassifier(use_label_encoder=False, eval_metric='mlogloss', random_state=42)
-scores = cross_val_score(model, features_df.drop(columns=['time']), y, cv=cv, scoring='accuracy')
+cv_model = XGBClassifier(use_label_encoder=False, eval_metric='mlogloss', random_state=42)
+cv_model.fit(features_df.drop(columns=['time']), y)
+scores = cross_val_score(cv_model, features_df.drop(columns=['time']), y, cv=cv, scoring='accuracy')
 print("CV scores: ", scores)
 print("Mean CV accuracy: ", scores.mean())
 
+# Testing model on new data set (test_raw1 and test_ann1)
+test_epochs = mne.make_fixed_length_epochs(test_raw1, duration=30.0, preload=True)
+test_psd = test_epochs.compute_psd(fmin=0.5, fmax=30, picks=['eeg', 'eog', 'emg'])
+test_epoch_midpoints = np.arange(len(test_epochs)) * 30.0 + 15.0
+test_labels = [stage_at_time(t, test_raw1.annotations) for t in test_epoch_midpoints]
+
+test_valid_mask = [lbl in class_map for lbl in test_labels]
+test_X_data = test_epochs.get_data()[test_valid_mask]
+test_y = np.array([class_map[lbl] for lbl in test_labels if lbl in class_map], dtype=int)
+test_times = test_epoch_midpoints[test_valid_mask]
+test_feature_rows = []
+for idx, epoch in enumerate(test_X_data):
+    row = {'time': test_times[idx]}
+    for ch_idx, ch_name in enumerate(test_epochs.ch_names):
+        data = epoch[ch_idx]
+        prefix = ch_name.replace(' ', '_').replace('-', '_')
+        row[f'{prefix}_mean'] = data.mean()
+        row[f'{prefix}_std'] = data.std()
+        row[f'{prefix}_min'] = data.min()
+        row[f'{prefix}_max'] = data.max()
+    test_feature_rows.append(row)
+
+test_features_df = pd.DataFrame(test_feature_rows)
+test_y_pred = cv_model.predict(test_features_df.drop(columns=['time']))
+test_accuracy = accuracy_score(test_y, test_y_pred)
+print(f'Test Accuracy: {test_accuracy:.4f}')
+#print(classification_report(test_y, test_y_pred))
+
 
 # ------------------------------------------HYPNOGRAM CREATION-------------------------------------
-
-raw_test = mne.io.read_raw_edf(f'{project_destination}/SC4012E0-PSG.edf', exclude=('Resp oro-nasal', 'Temp rectal','Event marker'))
-raw_test.set_channel_types({'EOG horizontal':'eog','EMG submental':'emg'})
-
-# the hypnogram.edf file only has annotations, must add to raw data
-ann_test = mne.read_annotations(f'{project_destination}/SC4012EC-Hypnogram.edf')
-
 # replace annotations (strings) with integers for yasa.from_integers function
-true_hypnogram = np.array([class_map.get(i,i) for i in ann_test.description])
+true_hypnogram = np.array([class_map.get(i,i) for i in test_ann1.description])
 
 # replace unknown sleep stages with
 true_hypnogram = [-2 if x == 'Sleep stage ?' else x for x in true_hypnogram]
 
+print(test_y_pred.shape)
+print(test_y.shape)
+print(len(true_hypnogram))
+
 # plot correct hypnogram
-hyp2 = Hypnogram.from_integers(true_hypnogram)
+hyp2 = Hypnogram.from_integers(test_y)
 hyp2.plot_hypnogram()
 plt.show()
-
-epoch_test = mne.make_fixed_length_epochs(raw_test, duration=30.0, preload=True)
